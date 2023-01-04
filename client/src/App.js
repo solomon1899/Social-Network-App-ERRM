4ee0e81cee90b4a9a8576c8eaf9b21895d9ff5aa
Soulaimane El khorchi <87861866+solomon1899@users.noreply.github.com>
2023-01-04 13:28:54 +0100

    2nd commit
    adding login register and alert components

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,40 @@
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
+import Alert from "./components/layout/Alert";
+import { useEffect } from "react";
+//Redux
+import { Provider } from "react-redux";
+import store from "./Store";
+import setAuthToken from "./utils/setAuthToken";
+import { loadUser } from "./actions/auth";
+
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
 
 const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route exact path="/" element={<Landing />} />
-        <Route className="container">
-          <Route exact path="/register" element={<Register />} />
-          <Route exact path="/login" element={<Login />} />
-        </Route>
-      </Routes>
-    </Router>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Navbar />
+        <Alert />
+        <Routes>
+          <Route exact path="/" element={<Landing />} />
+          <Route className="container">
+            <Route exact path="/register" element={<Register />} />
+            <Route exact path="/login" element={<Login />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   );
 };
 
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,6 +1,12 @@
 import { useState } from "react";
+import { connect } from "react-redux";
+import { setAlert } from "../../actions/alert";
+import { register } from "../../actions/auth";
+import { Navigate } from "react-router-dom";
+import "../../App.css";
+import PropTypes from "prop-types";
 
-const Register = () => {
+const Register = ({ setAlert, register, isAuhtenticated }) => {
   const [formData, setFormData] = useState({
     name: "",
     email: "",
@@ -14,72 +20,83 @@ const Register = () => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
-  const onSubmit = (e) => {
+  const onSubmit = async (e) => {
     e.preventDefault();
     if (password !== password2) {
-      console.log("password do not match");
-    } else console.log(formData);
+      setAlert("password do not match", "danger", 5000);
+    } else register({ name, email, password });
   };
-  return (
-    <div>
-      <section className="container">
-        <h1 className="large text-primary">Sign Up</h1>
-        <p className="lead">
-          <i className="fas fa-user"></i> Create Your Account
-        </p>
-        <form className="form" onSubmit={(e) => onSubmit(e)}>
-          <div className="form-group">
-            <input
-              type="text"
-              placeholder="Name"
-              name="name"
-              required
-              value={name}
-              onChange={(e) => onChange(e)}
-            />
-          </div>
-          <div className="form-group">
-            <input
-              value={email}
-              onChange={(e) => onChange(e)}
-              type="email"
-              placeholder="Email Address"
-              name="email"
-              required
-            />
-            <small className="form-text">
-              This site uses Gravatar so if you want a profile image, use a
-              Gravatar email
-            </small>
-          </div>
-          <div className="form-group">
-            <input
-              type="password"
-              placeholder="Password"
-              name="password"
-              minLength="6"
-              value={password}
-              onChange={(e) => onChange(e)}
-            />
-          </div>
-          <div className="form-group">
-            <input
-              type="password"
-              placeholder="Confirm Password"
-              name="password2"
-              minLength="6"
-              value={password2}
-              onChange={(e) => onChange(e)}
-            />
-          </div>
-          <input type="submit" className="btn btn-primary" value="Register" />
-        </form>
-        <p className="my-1">
-          Already have an account? <a href="login.html">Sign In</a>
-        </p>
-      </section>
-    </div>
-  );
+
+  if (isAuhtenticated) {
+    return <Navigate to="/dashboard" />;
+  } else
+    return (
+      <div>
+        <section className="container">
+          <h1 className="large text-primary">Sign Up</h1>
+          <p className="lead">
+            <i className="fas fa-user"></i> Create Your Account
+          </p>
+          <form className="form" onSubmit={(e) => onSubmit(e)}>
+            <div className="form-group">
+              <input
+                type="text"
+                placeholder="Name"
+                name="name"
+                value={name}
+                onChange={(e) => onChange(e)}
+              />
+            </div>
+            <div className="form-group">
+              <input
+                value={email}
+                onChange={(e) => onChange(e)}
+                type="email"
+                placeholder="Email Address"
+                name="email"
+              />
+              <small className="form-text">
+                This site uses Gravatar so if you want a profile image, use a
+                Gravatar email
+              </small>
+            </div>
+            <div className="form-group">
+              <input
+                type="password"
+                placeholder="Password"
+                name="password"
+                // minLength="6"
+                value={password}
+                onChange={(e) => onChange(e)}
+              />
+            </div>
+            <div className="form-group">
+              <input
+                type="password"
+                placeholder="Confirm Password"
+                name="password2"
+                // minLength="6"
+                value={password2}
+                onChange={(e) => onChange(e)}
+              />
+            </div>
+            <input type="submit" className="btn btn-primary" value="Register" />
+          </form>
+          <p className="my-1">
+            Already have an account? <a href="login.html">Sign In</a>
+          </p>
+        </section>
+      </div>
+    );
 };
 
-export default Register;
+const mapStateToProps = (state) => ({
+  isAuhtenticated: state.auth.isAuhtenticated,
+});
+
+Register.propTypes = {
+  setAlert: PropTypes.func.isRequired,
+  isAuhtenticated: PropTypes.bool,
+};
+
+export default connect(mapStateToProps, { setAlert, register })(Register);
